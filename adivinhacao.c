0bc2dc2d95196b301b4c3dc2091b8616e7ace53c
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-10 22:29:14 -0300

    Update adivinhacao.c

--- adivinhacao.c
+++ adivinhacao.c
@@ -5,4 +5,13 @@ int main() {
     printf("******************************************\n");
     printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
+
+    int numerosecreto = 42;
+
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+
 }
