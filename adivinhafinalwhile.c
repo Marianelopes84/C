924f5ec40aba6008bb0fce158545b4f21ab326cb
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-16 18:37:21 -0300

    Create adivinhafinalwhile.c

---
+++ adivinhafinalwhile.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    printf("************************************\n");
+    printf("* Bem vindo ao Jogo de Adivinhação *\n");
+    printf("************************************\n");
+
+    int chute;
+    int acertou = 0;
+    int tentativas = 1;
+    double pontos = 1000;
+
+    srand(time(0));
+    int numerosecreto = rand() % 100;
+
+    while(!acertou) {
+
+        printf("Qual é o seu %do. chute? ", tentativas);
+        scanf("%d", &chute);
+
+        if(chute < 0) {
+            printf("Você não pode chutar números negativos\n");
+            continue;
+        }
+
+        printf("Seu %do. chute foi %d\n", tentativas, chute);
+
+        acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+        } else if(maior) {
+            printf("Seu chute foi maior do que o número secreto!\n");
+        } else {
+            printf("Seu chute foi menor do que o número secreto!\n");
+        }
+
+        tentativas++;
+
+        double pontosperdidos = abs(chute - numerosecreto) / 2.0;
+        pontos = pontos - pontosperdidos;
+    }
+
+    printf("Você fez %.2f pontos", pontos);
+    printf("Obrigado por jogar!\n");
+
+}
