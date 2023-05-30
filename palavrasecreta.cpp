14d62842f026acc057174b4fc1a77e114cd2c340
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-30 16:46:58 -0300

    Add files via upload

---
+++ palavrasecreta.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char palavrasecreta [20];
+	sprintf(palavrasecreta, "MELANCIA");
+	
+	int acertou = 0;
+	int enforcou = 1;
+	
+	do{
+		char chute;
+		scanf("%c", &chute);
+		
+		for (int i = 0; i < strlen(palavrasecreta); i++){
+		
+			if (palavrasecreta[i] == chute) {
+			printf("A posição %d tem essa letra!\n", i);
+		}
+	} 
+	
+} while (!acertou && !enforcou);
+}
